80d5d28e6671ae05bd71ca5ad44d7218ca4b36c1
RZVD <razvanfuca@gmail.com>
2022-07-09 18:25:27 +0300

    Fixed colorpicker

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
 const INITIAL_GRID_DIMENSION = 16;
 const container = document.querySelector('.container');
+
+let mouseButton;
 let gridDimension = INITIAL_GRID_DIMENSION;
+let grid = [];
 let sliderText = document.createElement('p');
-sliderText.style.position = 'absolute';
-sliderText.textContent = gridDimension + 'X' + gridDimension;
 
 const sliderBox = document.querySelector('.slider_box');
-sliderBox.appendChild(sliderText);
-
 const slider = document.querySelector('.slider');
-console.log(slider.value);
 
-let grid = [];
-let mouseButton;
+document.body.onmousedown = () => (mouseButton = true);
+document.body.onmouseup = () => (mouseButton = false);
+
+const footer = document.querySelector('footer');
+
+const colorPicker = document.querySelector('#colorpicker');
+colorPicker.style.backgroundColor = colorPicker.value; 
+
+let colorButton= document.querySelector('.color');
+let rainbowButton= document.querySelector('.rainbow');
+let eraserButton= document.querySelector('.eraser');
+let clearButton= document.querySelector('.clear');
+
+
 
 function initalize(){
+    sliderText.style.position = 'absolute';
+    sliderText.textContent = gridDimension + 'X' + gridDimension;
+    sliderBox.appendChild(sliderText);
+    
+    
+    let copyright = 'Copyright© Fucă Răzvan ' + new Date().getFullYear();
+    let copyrightP = document.createElement('p');
+    copyrightP.textContent = copyright
+    footer.appendChild(copyrightP);
+    
     createGrid();
+
 }
 
 function placeSquareAndListen(square, row, column){
@@ -36,7 +57,7 @@ function placeSquareAndListen(square, row, column){
 }
 function changeColor(e) {
     if(e.type === 'mouseover' && !mouseButton) return;
-    e.target.style.backgroundColor = 'red' 
+    e.target.style.backgroundColor = 'red'; 
 }
 
 function createGrid(){
@@ -58,8 +79,6 @@ function destroyGrid(){
     }
     
 }
-document.body.onmousedown = () => (mouseButton = true);
-document.body.onmouseup = () => (mouseButton = false);
 
 slider.addEventListener('input', () =>{ 
     destroyGrid();
@@ -67,5 +86,12 @@ slider.addEventListener('input', () =>{
     sliderText.textContent = gridDimension + 'X' + gridDimension;
     createGrid();
 });
+
+colorPicker.addEventListener('input', () =>{
+    colorPicker.style.backgroundColor = colorPicker.value; 
+});
+
+
+
 
 initalize()
